6045338c8a96fd78d30853d9eb8a6ce335956a10
p.lukyanov <p.lukyanov@qa-academy.by>
2023-01-11 21:19:15 +0300

    add socket connection on the client, add server cors settings

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const socketEnable = require('socket.io')
 
 const app = express()
 const server = require('http').Server(app)
-
-const io = socketEnable(server)
+const io = require("socket.io")(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
 
 const rooms = new Map()
 
@@ -12,8 +15,8 @@ app.get('/rooms', (req, res) => {
     res.json(rooms)
 })
 
-io.on('connection', socket => {
-    console.log('user connected', socket)
+io.on('connection', client => {
+    console.log('user connected', client)
 })
 
 server.listen(9000, err => {
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import {io} from "socket.io-client";
+
+const socket = io('http://localhost:9000')
 
 export const App =() => {
   return (
     <div className="App">
+      HELLO
     </div>
   );
 }
